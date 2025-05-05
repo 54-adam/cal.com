b998847286a342d5bcc0949b605349cc6046342c
54-adam <zhyd1997@icloud.com>
2025-05-05 01:30:46 +0000

    build(apps/web): added release info into Sentry source maps.

--- apps/web/scripts/upload-sourcemaps.js
+++ apps/web/scripts/upload-sourcemaps.js
@@ -2,18 +2,28 @@ const { execSync } = require("child_process");
 
 console.log("RUNNING SENTRY SOURCEMAP UPLOAD... ", process.env.VERCEL);
 
-if (process.env.VERCEL) {
-  try {
-    console.log("Detected Vercel environment. Uploading Sentry sourcemaps...");
-    execSync(
-      // 'sentry-cli sourcemaps upload --help',
-      "sentry-cli sourcemaps inject .next/static/chunks && sentry-cli sourcemaps upload .next/static/chunks --validate --ext=js --ext=map",
-      { stdio: "inherit", env: process.env }
-    );
-  } catch (err) {
-    console.error("Sentry sourcemap upload failed:", err);
-    process.exit(1);
-  }
-} else {
-  console.log("Not in Vercel environment. Skipping Sentry sourcemap upload.");
+const SERVER_FILES_PATH = ".next/server";
+const CLIENT_FILES_PATH = ".next/static/chunks";
+
+try {
+  console.log("Detected Vercel environment. Uploading Sentry sourcemaps...");
+  const release = execSync("git rev-parse HEAD").toString().trim();
+
+  // Add release
+  execSync(`sentry-cli releases new ${release}`, { stdio: "inherit" });
+  // Inject Debug IDs
+  execSync(`sentry-cli sourcemaps inject ${CLIENT_FILES_PATH}`);
+  execSync(`sentry-cli sourcemaps inject ${SERVER_FILES_PATH}`);
+  // Upload
+  execSync(`sentry-cli sourcemaps upload ${CLIENT_FILES_PATH} --validate --ext=js --ext=map`, {
+    stdio: "inherit",
+    env: process.env,
+  });
+  execSync(`sentry-cli sourcemaps upload ${SERVER_FILES_PATH} --validate --ext=js --ext=map`, {
+    stdio: "inherit",
+    env: process.env,
+  });
+} catch (err) {
+  console.error("Sentry sourcemap upload failed:", err);
+  process.exit(1);
 }
